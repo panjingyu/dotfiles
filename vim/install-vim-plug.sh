ef7badb8208be079c78543f3e622ebe9b2d20b42
Jingyu Pan <jingyu.pan@duke.edu>
2021-10-31 02:52:57 -0400

    Add vim-plug installation script, update vimrc.

---
+++ vim/install-vim-plug.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Install vim-plug (https://github.com/junegunn/vim-plug)
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
