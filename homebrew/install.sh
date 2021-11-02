23e88c980dab555b0baef01a74053ecafd7af973
Jingyu Pan <jingyu.pan@duke.edu>
2021-11-02 04:06:08 -0400

    Update homebrew, add asking in install script.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-if ! command -v brew &> /dev/null
-then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
+install_homebrew='/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
 
+if command -v brew &> /dev/null
+then
+    while read -p 'Do you want to install Homebrew? [y/n]: ' ans
+    do
+        case $ans in
+            [Yy]* ) eval $install_homebrew; break;;
+            [Nn]* ) break;;
+            * ) echo 'Please answer yes or no.';;
+        esac
+    done
+fi
